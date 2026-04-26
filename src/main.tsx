096c408768f8cb41d17a9df5358f7dda5a17bdc3
juanenguerra-cmd <juan.enguerra@gmail.com>
2026-04-26 18:08:52 -0400

    Wire ErrorBoundary and global runtime logging

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { ErrorBoundary, installGlobalRuntimeLogging } from './components/ErrorBoundary';
+
+installGlobalRuntimeLogging();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </StrictMode>,
 );
