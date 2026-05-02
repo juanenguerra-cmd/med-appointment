f20a80168f57eb53bcfa5535d94e46debe08b21a
juanenguerra-cmd <juan.enguerra@gmail.com>
2026-05-02 19:56:01 -0400

    Expand component barrel exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,5 @@ export { Button } from "./Button";
 export { Card } from "./Card";
 export { CurrentReleaseNote } from "./CurrentReleaseNote";
 export { VersionHistoryPanel } from "./VersionHistoryPanel";
+export { LockScreen } from "./LockScreen";
+export { AppointmentModal } from "./appointments/AppointmentModal";
