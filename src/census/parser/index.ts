517c7d7cd761acdafeabbb3a8dd2d3c4bad5a21c
juanenguerra-cmd <juan.enguerra@gmail.com>
2026-05-05 21:17:54 -0400

    Add census parser barrel export

---
+++ src/census/parser/index.ts
@@ -0,0 +1,7 @@
+export * from "./censusTypes";
+export * from "./normalizeRawCensusText";
+export * from "./fieldExtractors";
+export * from "./duplicateDetection";
+export * from "./cleanCensusRows";
+export * from "./censusReconciliation";
+export * from "./parseCensusText";
